46960d151fb2bf7d1e718ebad2f4f29fa8e66c7e
aci456852 <1084665808@qq.com>
2021-06-25 09:51:12 +0800

    1. Migrate your type hint to generics 2. Add possibility to use your hashmap as OrderedDict

--- src/Lab1/immutable.py
+++ src/Lab1/immutable.py
@@ -2,9 +2,14 @@
 """
 Author: Liang Ziyi
 Date: 2021/3/20
-Title: Dictionary based on hash-map (immutable)
+Title: Dictionary based on od-map (immutable)
 """
 from typing import List, Tuple, Union, Optional
+from typing import TypeVar
+import collections
+
+T = TypeVar('T', int, str, bytes, bool)
+
 
 class Node(object):
     def __init__(self, key=None, value=None, next=None) -> None:
@@ -15,33 +20,33 @@ class Node(object):
     def __repr__(self) -> str:
         return "Node [key=" + self.key + ",value=" + self.value + "]"
 
-class HashMap(object):
+class OrderedDict(collections.OrderedDict):
 
-    def __init__(self, dict=None) -> None:
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.move_to_end(key)
+
+    def __init__(self) -> None:
         self.key_set: List[int] = []
         self.size = 10
         self.data = [Node() for _ in range(10)]
 
-        # Initialize dict
-        if dict is not None:
-            self.from_dict(self, dict)
+
+# get od value
+def get_value(od: OrderedDict, key: T) -> int:
+    od_value = key % od.size
+    return od_value
 
 
-# get hash value
-def get_value(hash: HashMap, key: int) -> int:
-    hash_value = key % hash.size
-    return hash_value
-
-
-# insert key-value pairs into hash map
-def add(hash: HashMap, key: Union[int, str, bytes, bool], value: int) -> HashMap:
+# insert key-value pairs into od map
+def add(od: OrderedDict, key: T, value: int) -> OrderedDict:
     # copy structure
-    new_hash = HashMap()
-    new_hash.size = hash.size
-    for x in range(len(hash.key_set)):
-        new_hash.key_set.append(hash.key_set[x])
-    for x in range(len(hash.data)):
-        new_hash.data[x] = hash.data[x]
+    new_od = OrderedDict()
+    new_od.size = od.size
+    for x in range(len(od.key_set)):
+        new_od.key_set.append(od.key_set[x])
+    for x in range(len(od.data)):
+        new_od.data[x] = od.data[x]
 
     if type(key) == str:  # change key's type for remove key type restriction --str
         key = ord(key)
@@ -53,44 +58,44 @@ def add(hash: HashMap, key: Union[int, str, bytes, bool], value: int) -> HashMap
     if type(key) == float:  # change key's type for remove key type restriction --float
         key = int(key)
 
-    hash_value = get_value(new_hash, key)
-    if new_hash is None:
-        new_hash = HashMap()
-    if new_hash.data[hash_value].key is None:
-        new_hash.data[hash_value].value = value
-        new_hash.data[hash_value].key = key
-        new_hash.key_set.append(key)
+    od_value = get_value(new_od, key)
+    if new_od is None:
+        new_od = OrderedDict()
+    if new_od.data[od_value].key is None:
+        new_od.data[od_value].value = value
+        new_od.data[od_value].key = key
+        new_od.key_set.append(key)
     else:
         temp = Node(key, value)
-        new_hash.key_set.append(key)
-        p = new_hash.data[hash_value]
+        new_od.key_set.append(key)
+        p = new_od.data[od_value]
         while p.next is not None:
             p = p.next
         p.next = temp
-    return new_hash
+    return new_od
 
 
-# remove element in hash map by key
-def remove(hash, key) -> HashMap:
+# remove element in od map by key
+def remove(od: OrderedDict, key: T) -> OrderedDict:
     # Add tests for exceptions
     if key is None:
         raise Exception("remove not existence key!")
 
     # copy structure
-    new_hash = HashMap()
-    new_hash.size = hash.size
-    for x in range(len(hash.key_set)):
-        new_hash.key_set.append(hash.key_set[x])
-    for x in range(len(hash.data)):
-        new_hash.data[x] = hash.data[x]
+    new_od = OrderedDict()
+    new_od.size = od.size
+    for x in range(len(od.key_set)):
+        new_od.key_set.append(od.key_set[x])
+    for x in range(len(od.data)):
+        new_od.data[x] = od.data[x]
 
-    if new_hash is None:
-        raise Exception("new_hash is None!")
-    hash_value = get_value(new_hash, key)
-    if new_hash.data[hash_value].value is None:
+    if new_od is None:
+        raise Exception("new_od is None!")
+    od_value = get_value(new_od, key)
+    if new_od.data[od_value].value is None:
         raise Exception('No valid key value was found')
     else:
-        p = new_hash.data[hash_value]
+        p = new_od.data[od_value]
         if key == p.key:
             if p.next is None:
                 p.key = None
@@ -100,22 +105,22 @@ def remove(hash, key) -> HashMap:
                 p.key = temp.key
                 p.value = temp.value
                 p.next = temp.next
-            remove_key_set(new_hash, key)
-            return new_hash
+            remove_key_set(new_od, key)
+            return new_od
         else:
             while p is not None:
                 if p.key == key:
                     temp = p.next
                     p.next = temp.next
-                    remove_key_set(new_hash, key)
-                    return new_hash
+                    remove_key_set(new_od, key)
+                    return new_od
                 else:
                     p = p.next
     raise Exception('No valid key value was found')
 
 
-# find element in hash map by key
-def find(hash: HashMap, key: int) -> object:
+# find element in od map by key
+def find(od: OrderedDict, key: T) -> object:
     if type(key) == str:  # change key's type for remove key type restriction --str
         key = ord(key)
     if type(key) == bool:  # change key's type for remove key type restriction --bool
@@ -126,15 +131,15 @@ def find(hash: HashMap, key: int) -> object:
     if type(key) == float:  # change key's type for remove key type restriction --float
         key = int(key)
 
-    if hash.key_set is None:
+    if od.key_set is None:
         return None
     i = 0
-    while i < hash.size:
-        if hash.data[i].key is None:
+    while i < od.size:
+        if od.data[i].key is None:
             i += 1
             continue
         else:
-            p = hash.data[i]
+            p = od.data[i]
             while p is not None:
                 if p.key == key:
                     return p.value
@@ -143,54 +148,54 @@ def find(hash: HashMap, key: int) -> object:
     raise Exception("can not find the key")
 
 
-def remove_key_set(hash: HashMap, key: int) -> HashMap:
+def remove_key_set(od: OrderedDict, key: T) -> OrderedDict:
     # Add tests for exceptions
     if key is None:
         raise Exception("remove not existence key!")
 
     # copy structure
-    new_hash = HashMap()
-    new_hash.size = hash.size
-    for x in range(len(hash.key_set)):
-        new_hash.key_set.append(hash.key_set[x])
-    for x in range(len(hash.data)):
-        new_hash.data[x] = hash.data[x]
+    new_od = OrderedDict()
+    new_od.size = od.size
+    for x in range(len(od.key_set)):
+        new_od.key_set.append(od.key_set[x])
+    for x in range(len(od.data)):
+        new_od.data[x] = od.data[x]
 
-    for i, k in enumerate(new_hash.key_set):
+    for i, k in enumerate(new_od.key_set):
         if key == k:
-            arr = new_hash.key_set
+            arr = new_od.key_set
             del arr[i]
-            return new_hash
+            return new_od
 
     raise Exception("can not find the key")
 
 
-def from_dict(hash: HashMap, dict: Optional[dict]) -> HashMap:
-    new_hash = HashMap()
-    new_hash.size = hash.size
-    for x in range(len(hash.key_set)):
-        new_hash.key_set.append(hash.key_set[x])
-    for x in range(len(hash.data)):
-        new_hash.data[x] = hash.data[x]
+def from_dict(od: OrderedDict, dict: Optional[dict]) -> OrderedDict:
+    new_od = OrderedDict()
+    new_od.size = od.size
+    for x in range(len(od.key_set)):
+        new_od.key_set.append(od.key_set[x])
+    for x in range(len(od.data)):
+        new_od.data[x] = od.data[x]
 
     for key, value in dict.items():
-        new_hash = add(new_hash, key, value)
+        new_od = add(new_od, key, value)
 
-    return new_hash
+    return new_od
 
-# transfer hash map into dict
-def to_dict(hash: HashMap) -> {}:
+# transfer od map into dict
+def to_dict(od: OrderedDict) -> {}:
     myDict = {}
-    if hash is None:
+    if od is None:
         return myDict
     else:
         i = 0
-        while i < hash.size:
-            if hash.data[i].value is None:
+        while i < od.size:
+            if od.data[i].value is None:
                 i += 1
                 continue
             else:
-                p = hash.data[i]
+                p = od.data[i]
                 while p is not None:
                     myDict[p.key] = p.value
                     p = p.next
@@ -198,16 +203,16 @@ def to_dict(hash: HashMap) -> {}:
     return myDict
 
 
-# element number in hash map
-def get_size(hash: HashMap) -> int:
+# element number in od map
+def get_size(od: OrderedDict) -> int:
     sum = 0
     i = 0
-    while i < hash.size:
-        if hash.data[i].value is None:
+    while i < od.size:
+        if od.data[i].value is None:
             i += 1
             continue
         else:
-            p = hash.data[i]
+            p = od.data[i]
             while p is not None:
                 sum += 1
                 p = p.next
@@ -215,32 +220,32 @@ def get_size(hash: HashMap) -> int:
     return sum
 
 
-# transfer hash map into list type
-def to_list(hash: HashMap) -> List:
+# transfer od map into list type
+def to_list(od: OrderedDict) -> List:
     list = []
-    if hash is None:
+    if od is None:
         return list
-    for i, key in enumerate(hash.key_set):
-        list.append(find(hash, key))
+    for i, key in enumerate(od.key_set):
+        list.append(find(od, key))
     return list
 
 
 # add element from list type
-def from_list(hash: HashMap, list: List) -> HashMap:
-    new_hash = HashMap()
-    new_hash.size = hash.size
-    for x in range(len(hash.key_set)):
-        new_hash.key_set.append(hash.key_set[x])
-    for x in range(len(hash.data)):
-        new_hash.data[x] = hash.data[x]
+def from_list(od: OrderedDict, list: List) -> OrderedDict:
+    new_od = OrderedDict()
+    new_od.size = od.size
+    for x in range(len(od.key_set)):
+        new_od.key_set.append(od.key_set[x])
+    for x in range(len(od.data)):
+        new_od.data[x] = od.data[x]
 
     for key, value in enumerate(list):
-        new_hash = add(new_hash, key, value)
-    return new_hash
+        new_od = add(new_od, key, value)
+    return new_od
 
-# find element with even value in hash map.
-def find_iseven(hash: HashMap) -> HashMap:
-    list = to_list(hash)
+# find element with even value in od map.
+def find_iseven(od: OrderedDict) -> OrderedDict:
+    list = to_list(od)
     my_list = []
     for value in range(len(list)):
         if type(list[value]) is int or type(list[value]) is float:
@@ -249,9 +254,9 @@ def find_iseven(hash: HashMap) -> HashMap:
     return my_list
 
 
-# filter element with even value in hash map.
-def filter_iseven(hash: HashMap) -> List:
-    list = to_list(hash)
+# filter element with even value in od map.
+def filter_iseven(od: OrderedDict) -> List:
+    list = to_list(od)
     for value in list:
         if type(value) is int or type(value) is float:
             if value % 2 == 0:
@@ -259,29 +264,29 @@ def filter_iseven(hash: HashMap) -> List:
     return list
 
 
-# map element value in hash map with f
-def map(hash: HashMap, f: type) -> dict:
-    dict = to_dict(hash)
-    for key in hash.key_set:
-        value = f(find(hash, key))
+# map element value in od map with f
+def map(od: OrderedDict, f: type) -> dict:
+    dict = to_dict(od)
+    for key in od.key_set:
+        value = f(find(od, key))
         dict[key] = value
     return dict
 
 
 #  build a return value by specific functions(f)
-def reduce(hash: HashMap, f, initial_state):
+def reduce(od: OrderedDict, f, initial_state):
     state = initial_state
-    for key in hash.key_set:
-        value = find(hash, key)
+    for key in od.key_set:
+        value = find(od, key)
         state = f(state, value)
     return state
 
 
-def mempty(hash) -> None:
+def mempty(od) -> None:
     return None
 
 
-def mconcat(a: HashMap, b: HashMap) -> HashMap:
+def mconcat(a: OrderedDict, b: OrderedDict) -> OrderedDict:
     if a is None:
         return b
     if b is None:
@@ -293,10 +298,10 @@ def mconcat(a: HashMap, b: HashMap) -> HashMap:
 
 
 # iterator
-def iterator(hash: HashMap) -> object:
-    if hash is not None:
+def iterator(od: OrderedDict) -> object:
+    if od is not None:
         res = []
-        list = to_list(hash)
+        list = to_list(od)
         for i in list:
             res.append(i)
         a = iter(res)
--- src/Lab1/test_immutable.py
+++ src/Lab1/test_immutable.py
@@ -3,69 +3,70 @@ from src.Lab1.immutable import *
 from hypothesis import given
 import hypothesis.strategies as st
 import pytest
+import collections
 
 def test_get_value():
-    hash = HashMap()
-    assert get_value(hash, 5) == 5
-    assert get_value(hash, 24) == 4
+    od = OrderedDict()
+    assert get_value(od, 5) == 5
+    assert get_value(od, 24) == 4
 
 def test_add():
-    hash = HashMap()
-    hash = add(hash, 2, 1)
-    assert find(hash, 2) == 1
-    assert to_dict(hash) == {2: 1}
-    hash = add(hash, 4, 2)
-    assert find(hash, 4) ==2
-    hash = add(hash, "A", 3)
-    assert find(hash, "A") == 3
-    hash = add(hash, True, 4)
-    assert find(hash, True) == 4
-    hash = add(hash, False, 5)
-    assert find(hash, False) == 5
-    hash = add(hash, 3.14, 6)
-    assert find(hash, 3.14) == 6
+    od = OrderedDict()
+    od = add(od, 2, 1)
+    assert find(od, 2) == 1
+    assert to_dict(od) == {2: 1}
+    od = add(od, 4, 2)
+    assert find(od, 4) ==2
+    od = add(od, "A", 3)
+    assert find(od, "A") == 3
+    od = add(od, True, 4)
+    assert find(od, True) == 4
+    od = add(od, False, 5)
+    assert find(od, False) == 5
+    od = add(od, 3.14, 6)
+    assert find(od, 3.14) == 6
 
 def test_remove():
-    hash = HashMap()
+    od = OrderedDict()
     dict1 = {1: 2, 2: 4, 3: 6}
-    hash = from_dict(hash, dict1)
-    remove(hash, 1)
+    od = from_dict(od, dict1)
+    remove(od, 1)
     with pytest.raises(Exception):  # Exception detection
-        remove(hash, 5)
+        remove(od, 5)
     dict2 = {2: 4, 3: 6}
-    assert to_dict(hash) == dict2
+    assert to_dict(od) == dict2
 
 def test_remove_key_set():
-    hash = HashMap()
-    assert hash.key_set == []
-    hash = from_dict(hash, {1: 2, 2: 4, 3: 6})
-    assert hash.key_set == [1, 2, 3]
-    hash = remove_key_set(hash, 1)
-    assert hash.key_set == [2, 3]
+    od = OrderedDict()
+    assert od.key_set == []
+    od = from_dict(od, {1: 2, 2: 4, 3: 6})
+    assert od.key_set == [1, 2, 3]
+    od = remove_key_set(od, 1)
+    assert od.key_set == [2, 3]
 
 def test_from_dict():
-    hash = HashMap()
+    od = OrderedDict()
     dict = {1: 2, 2: 4, 3: 6, 4: 8}
-    from_dict(hash, dict)
-    assert find(hash, 4) == 8
-    assert find(hash, 3) == 6
+    from_dict(od, dict)
+    assert find(od, 4) == 8
+    assert find(od, 3) == 6
 
 def test_to_dict():
-    hash = HashMap()
-    hash = add(hash, 1, 2)
-    hash = add(hash, 2, 4)
-    hash = add(hash, 3, 6)
-    hash = add(hash, 4, 8)
-    to_dict(hash)
-    assert to_dict(hash) == {1: 2, 2: 4, 3: 6, 4: 8}
+    od = OrderedDict()
+    od = add(od, 1, 2)
+    od = add(od, 2, 4)
+    od = add(od, 3, 6)
+    od = add(od, 4, 8)
+    to_dict(od)
+    assert to_dict(od) == {1: 2, 2: 4, 3: 6, 4: 8}
 
 def test_get_size():
-    hash = HashMap()
-    assert get_size(hash) == 0
-    hash = add(hash, 1, 2)
-    assert get_size(hash) == 1
-    hash = add(hash, 14, 2)
-    assert get_size(hash) == 2
+    od = OrderedDict()
+    assert get_size(od) == 0
+    od = add(od, 1, 2)
+    assert get_size(od) == 1
+    od = add(od, 14, 2)
+    assert get_size(od) == 2
 
 def test_from_list():
     test_data = [
@@ -76,129 +77,129 @@ def test_from_list():
         [None]
     ]
     for e in test_data:
-        hash = HashMap()
-        hash = from_list(hash, e)
-        assert to_list(hash) == e
+        od = OrderedDict()
+        od = from_list(od, e)
+        assert to_list(od) == e
 
 def test_to_list():
-    hash = HashMap()
+    od = OrderedDict()
     dict = {1: 2, 2: 4, 3: 6, 4: 8}
-    hash = from_dict(hash, dict)
-    assert to_list(hash) == [2, 4, 6, 8]
+    od = from_dict(od, dict)
+    assert to_list(od) == [2, 4, 6, 8]
 
 def test_find_iseven():
-    hash = HashMap()
-    hash = from_list(hash, ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8])
-    assert to_list(hash) == ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8]
-    assert find_iseven(hash) == [2, 4, 6.0, 8]
+    od = OrderedDict()
+    od = from_list(od, ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8])
+    assert to_list(od) == ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8]
+    assert find_iseven(od) == [2, 4, 6.0, 8]
 
 def test_filter_iseven():
-    hash = HashMap()
-    hash = from_list(hash, ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8])
-    assert to_list(hash) == ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8]
-    assert filter_iseven(hash) == ['a', 1, 3.14, 'b', 5, 'c', 7]
+    od = OrderedDict()
+    od = from_list(od, ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8])
+    assert to_list(od) == ['a', 1, 2, 3.14, 'b', 4, 5, 'c', 6.0, 7, 8]
+    assert filter_iseven(od) == ['a', 1, 3.14, 'b', 5, 'c', 7]
 
 def test_map():
     dict1 = {1: 2, 2: 4}
     dict2 = {1: '2', 2: '4'}
-    hash = HashMap()
-    hash = from_dict(hash, dict1)
-    assert map(hash, str) == dict2
+    od = OrderedDict()
+    od = from_dict(od, dict1)
+    assert map(od, str) == dict2
 
 def test_reduce():
-    hash = HashMap()
-    assert reduce(hash, lambda st, e: st + e, 0) == 0
+    od = OrderedDict()
+    assert reduce(od, lambda st, e: st + e, 0) == 0
     dict1 = {1: 2, 2: 4}
-    hash1 = HashMap()
-    hash1 = from_dict(hash1, dict1)
-    assert reduce(hash1, lambda st, e: st + e, 0) == 6
+    od1 = OrderedDict()
+    od1 = from_dict(od1, dict1)
+    assert reduce(od1, lambda st, e: st + e, 0) == 6
 
-def test_hash_collision():
-    hash = HashMap()
-    hash = add(hash, 1, 7)
-    hash = add(hash, 11, 777)
-    assert get_value(hash, 1) == get_value(hash, 11)  # check input two keys have the same hash
-    assert find(hash, 1) == 7  # check that data inside your structure store well
-    assert find(hash, 11) == 777
+def test_od_collision():
+    od = OrderedDict()
+    od = add(od, 1, 7)
+    od = add(od, 11, 777)
+    assert get_value(od, 1) == get_value(od, 11)  # check input two keys have the same od
+    assert find(od, 1) == 7  # check that data inside your structure store well
+    assert find(od, 11) == 777
 
 def test_iter():
     dict1 = {1: 2, 2: 4, 3: 6, 4: 8}
-    hash = HashMap()
-    hash = from_dict(hash, dict1)
+    od = OrderedDict()
+    od = from_dict(od, dict1)
     tmp = {}
-    for e in hash:
+    for e in od:
         tmp[e.key] = e.value
-    assert to_dict(hash) == tmp
-    i = iter(HashMap())
+    assert to_dict(od) == tmp
+    i = iter(OrderedDict())
     pytest.raises(StopIteration, lambda: next(i))
 
 # e·a = a·e = acollision
 @given(a=st.lists(st.integers()))
 def test_monoid_identity(a):
-    hash = HashMap()
-    hash_a = HashMap()
-    hash_a = from_list(hash_a, a)
-    assert mconcat(mempty(hash), hash_a) == hash_a
-    assert mconcat(hash_a, mempty(hash)) == hash_a
+    od = OrderedDict()
+    od_a = OrderedDict()
+    od_a = from_list(od_a, a)
+    assert mconcat(mempty(od), od_a) == od_a
+    assert mconcat(od_a, mempty(od)) == od_a
 
 # (a·b)·c = a·(b·c)
 @given(a=st.lists(st.integers()), b=st.lists(st.integers()), c=st.lists(st.integers()))
 def test_monoid_associativity(a, b, c):
-    hash_a = HashMap()
-    hash_b = HashMap()
-    hash_c = HashMap()
-    hash_a = from_list(hash_a, a)
-    hash_b = from_list(hash_b, b)
-    hash_c = from_list(hash_c, c)
+    od_a = OrderedDict()
+    od_b = OrderedDict()
+    od_c = OrderedDict()
+    od_a = from_list(od_a, a)
+    od_b = from_list(od_b, b)
+    od_c = from_list(od_c, c)
     # (a·b)·c
-    a_b = mconcat(hash_a, hash_b)
-    ab_c = mconcat(a_b, hash_c)
+    a_b = mconcat(od_a, od_b)
+    ab_c = mconcat(a_b, od_c)
 
-    hash_a1 = HashMap()
-    hash_b1 = HashMap()
-    hash_c1 = HashMap()
-    hash_a1 = from_list(hash_a1, a)
-    hash_b1 = from_list(hash_b1, b)
-    hash_c1 = from_list(hash_c1, c)
+    od_a1 = OrderedDict()
+    od_b1 = OrderedDict()
+    od_c1 = OrderedDict()
+    od_a1 = from_list(od_a1, a)
+    od_b1 = from_list(od_b1, b)
+    od_c1 = from_list(od_c1, c)
     # a·(b·c)
-    b_c = mconcat(hash_b1, hash_c1)
-    a_bc = mconcat(hash_a1, b_c)
+    b_c = mconcat(od_b1, od_c1)
+    a_bc = mconcat(od_a1, b_c)
 
     assert id(ab_c) != id(a_bc)  # address is not same
     assert to_list(ab_c) == to_list(a_bc)  # value is same
 
 @given(st.lists(st.integers()))
 def test_from_list_to_list_equality(a):
-    hash = HashMap()
-    hash = from_list(hash, a)
-    b = to_list(hash)
+    od = OrderedDict()
+    od = from_list(od, a)
+    b = to_list(od)
     assert a == b
 
 @given(st.lists(st.integers()))
 def test_python_len_and_list_size_equality(a):
-    hash = HashMap()
-    hash = from_list(hash, a)
-    assert get_size(hash) == len(a)
+    od = OrderedDict()
+    od = from_list(od, a)
+    assert get_size(od) == len(a)
 
 @given(st.lists(st.integers()))
 def test_from_list(a):
-    hash = HashMap()
-    hash = from_list(hash, a)
-    assert to_list(hash) == a
+    od = OrderedDict()
+    od = from_list(od, a)
+    assert to_list(od) == a
 
 def test_iter():
     x = [1, 2, 3]
-    hash = HashMap()
-    hash = from_list(hash, x)
+    od = OrderedDict()
+    od = from_list(od, x)
     tmp = []
     try:
-        get_next = iterator(hash)
+        get_next = iterator(od)
         while True:
             tmp.append(get_next())
     except StopIteration:
         pass
     assert x == tmp
-    assert to_list(hash) == tmp
+    assert to_list(od) == tmp
 
     get_next = iterator(None)
     assert get_next() == False
